71fd55ba6437ae45fd49f044e06c7ae53d7c27f5
Nephter <nephter@gmail.com>
2021-03-08 18:56:02 -0600

    s

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 /* eslint-disable no-unused-expressions */
-import React from "react"; 3.4
+import React from "react";
+import Tweet from "./Tweet";
 
 function App() {
+
     return (
        <div>
            <h1>Hello React</h1>
---
+++ src/Tweet.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Tweet(){
+    return(
+        <div>
+            <h3>Name</h3>
+            <p>This is a random tweet</p>
+            <h3>Number of Likes</h3>
+        </div>
+    )
+}
+
+export default Tweet;
